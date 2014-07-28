31d486832f5146461bb6929d23024d01eed9a5f7
Tay <me@taylorrome.ro>
2014-07-28 10:49:56 -0600

    widget rendering extension

--- extensions/Widget.js
+++ extensions/Widget.js
@@ -30,16 +30,16 @@ define(['altair/facades/declare',
                             _config = config || {};
 
                         //default place to look for templates
-                        if(!_options.viewPaths) {
+                        if (!_options.viewPaths) {
                             _options.viewPaths = [this.resolvePath(this.viewPath || 'views')];
                         }
 
                         //if it's a nexus name, pass it off
-                        if(named.search(':') > 0) {
+                        if (named.search(':') > 0) {
                             return this.nexus(named, _options, _config);
                         }
 
-                        if(named.search(/\./) === -1) {
+                        if (named.search(/\./) === -1) {
                             dfd = new this.Deferred();
                             dfd.reject('You need to give your widget an instance id: this.widget(\'Name.instanceId\') -> this.widget(\'liquidfire:Forms/widgets/Form.create-user\')');
                         } else {
@@ -59,7 +59,7 @@ define(['altair/facades/declare',
 
                             dfd = this.forge(path, _options, _config).then(function (widget) {
                                 widget.instanceId = instanceId;
-                                return widget;
+                                return _config.render ? widget.render(_config.template) : widget;
                             });
                         }
 
